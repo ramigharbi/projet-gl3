ecdc29075cfc13ec2fe95ee2a458fc157dc56dbc
boukamcha <adem.letaiefboukamcha@insat.ucar.tn>
2025-06-01 01:27:26 +0100

    refactor

--- front/src/graphql/comments.js
+++ front/src/graphql/comments.js
@@ -1,23 +1,38 @@
-import { gql } from '@apollo/client';
-
-// Query to get all comments for a document
-export const GET_COMMENTS = gql`
-  query GetComments($docId: String!) {
+// GraphQL Queries and Subscriptions
+const GET_COMMENTS = gql`
+  query GetComments($docId: String!) { # Changed ID! to String!
     comments(docId: $docId) {
       commentId
       docId
-      text
       author
-      createdAt
-      updatedAt
+      text
       rangeStart
       rangeEnd
+      createdAt
+      updatedAt
     }
   }
 `;
 
-// Mutation to add a new comment
-export const ADD_COMMENT = gql`
+const COMMENT_EVENTS = gql`
+  subscription CommentEvent($docId: String!) {
+    commentEvent(docId: $docId) {
+      type
+      comment {
+        commentId
+        docId
+        author
+        text
+        rangeStart
+        rangeEnd
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+
+const ADD_COMMENT = gql`
   mutation AddComment($docId: String!, $input: CommentInput!) {
     addComment(docId: $docId, input: $input) {
       comment {
@@ -35,49 +50,29 @@ export const ADD_COMMENT = gql`
   }
 `;
 
-// Mutation to update an existing comment
-export const UPDATE_COMMENT = gql`
-  mutation UpdateComment($docId: String!, $commentId: String!, $input: CommentInput!) {
-    updateComment(docId: $docId, commentId: $commentId, input: $input) {
-      comment {
-        commentId
-        docId
-        text
-        author
-        createdAt
-        updatedAt
-        rangeStart
-        rangeEnd
-      }
-      message
-    }
-  }
-`;
 
-// Mutation to delete a comment
-export const DELETE_COMMENT = gql`
+const DELETE_COMMENT = gql`
   mutation DeleteComment($docId: String!, $commentId: String!) {
-    deleteComment(docId: $docId, commentId: $commentId)
-  }
-`;
-
-// Subscription to listen for comment events
-export const COMMENT_EVENTS = gql`
-  subscription CommentEvent($docId: String!) {
-    commentEvent(docId: $docId) {
-      type
+    deleteComment(docId: $docId, commentId: $commentId){
       comment {
         commentId
         docId
-        author
         text
-        rangeStart
-        rangeEnd
+        author
         createdAt
         updatedAt
+        rangeStart
+        rangeEnd
       }
-      docId
-      commentId
+      message 
     }
   }
 `;
+
+
+export default{
+  GET_COMMENTS,
+  COMMENT_EVENTS,
+  ADD_COMMENT,
+  DELETE_COMMENT
+}
